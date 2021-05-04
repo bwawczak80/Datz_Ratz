7ed8a91c1619eefcb4d5b95340445d0334fd9b18
bwawczak80 <bwawczak@students.mchenry.edu>
2021-05-03 20:20:26 -0500

    Added User Authentication, Forgot Password feature, and splash screen.

--- app/src/main/java/com/bwawczak/datz_ratz/firestore/FirestoreClass.kt
+++ app/src/main/java/com/bwawczak/datz_ratz/firestore/FirestoreClass.kt
@@ -1,9 +1,57 @@
 package com.bwawczak.datz_ratz.firestore
 
+import android.util.Log
+import com.bwawczak.datz_ratz.AddSnakeFragment
+import com.bwawczak.datz_ratz.RegisterActivity
+import com.bwawczak.datz_ratz.models.User
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.SetOptions
 
 class FirestoreClass {
 
 
     private val mFireStore = FirebaseFirestore.getInstance()
+
+    fun registerUser(activity: RegisterActivity, userInfo: User) {
+
+        //this will create the collection "users" the first time it is run.
+        mFireStore.collection("users")
+        //Document ID for users fields.
+            .document(userInfo.id)
+            .set(userInfo, SetOptions.merge())
+            .addOnSuccessListener {
+
+                activity.userRegistrationSuccess()
+
+            }
+            .addOnFailureListener { e ->
+                Log.e(activity.javaClass.simpleName,"Error while registering the user.",e)
+            }
+    }
+
+    fun addSnake(activity: AddSnakeFragment, userInfo: User) {
+        mFireStore.collection("users").document(userInfo.id).collection("snakes")
+            .set(userInfo, SetOptions.merge()).addOnSuccessListener {
+                activity.
+            }
+    }
+
+
+
+
+// get CurrentUser from Authentication module
+fun getCurrentUserId(): String {
+    //gets and instance of the currentUser
+    val currentUser = FirebaseAuth.getInstance().currentUser
+
+    var currentUserID = ""
+    if (currentUser != null) {
+        currentUserID = currentUser.uid
+    }
+
+    return currentUserID
+}
+
+
 }
