48d68e4dbab7c680eca29228280fd67078f49365
bwawczak80 <bwawczak@students.mchenry.edu>
2021-05-04 22:36:45 -0500

    Added snakeLog screen and clickable RecyclerView

--- app/src/main/java/com/bwawczak/datz_ratz/AddSnakeLog.kt
+++ app/src/main/java/com/bwawczak/datz_ratz/AddSnakeLog.kt
@@ -1,15 +1,17 @@
 package com.bwawczak.datz_ratz
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bwawczak.datz_ratz.models.LogItem
 import kotlinx.android.synthetic.main.activity_add_snake_log.*
-import kotlinx.android.synthetic.main.activity_add_snake_log.autoCompleteTextView
-import kotlinx.android.synthetic.main.fragment_add_snake.*
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -21,12 +23,19 @@ class AddSnakeLog : AppCompatActivity() {
 
         val logs = arrayOf<LogItem>()
 
+        //create arrayAdapter for custom drop down menu
+
+        val rodentList = resources.getStringArray(R.array.rodents)
+        val arrayAdapter = ArrayAdapter(this, R.layout.dropdown_item, rodentList)
+        this.autoCompleteTextViewLog.setAdapter(arrayAdapter)
+
+
         logBtn.setOnClickListener {
             //get user input data
             //verify and trim user data
             when {
 
-                TextUtils.isEmpty(weightInput.text.toString().trim { it <= ' ' }) -> {
+                TextUtils.isEmpty(weightLogInput.text.toString().trim { it <= ' ' }) -> {
                     Toast.makeText(
                         this@AddSnakeLog,
                         "Please enter your snakes weight.",
@@ -34,7 +43,7 @@ class AddSnakeLog : AppCompatActivity() {
                     ).show()
                 }
 
-                TextUtils.isEmpty(autoCompleteTextView.text.toString().trim { it <= ' ' }) -> {
+                TextUtils.isEmpty(autoCompleteTextViewLog.text.toString().trim { it <= ' ' }) -> {
                     Toast.makeText(
                         this@AddSnakeLog,
                         "Please choose a rodent.",
@@ -43,22 +52,26 @@ class AddSnakeLog : AppCompatActivity() {
                 }
                 else -> {
                     //get date and time stamp
-                    val datePattern ="dd-MM-yyyy"
+                    val datePattern ="dd/MM/yyyy"
                     val simpleDateFormat = SimpleDateFormat(datePattern)
                     val date = simpleDateFormat.format(Date())
 
+                    val isInShed:Boolean = checkBox.isChecked
+
 
                     val logs = LogItem(
-                        weightInput.text.toString().trim { it <= ' ' },
+                        weightLogInput.text.toString().trim { it <= ' ' },
                         date,
-                        autoCompleteTextView.text.toString()
+                        autoCompleteTextViewLog.text.toString(),
+                        isInShed
+
                     )
                     //transition back to homeFragment
                     val intent =
                         Intent(this@AddSnakeLog, MainActivity::class.java)
                     intent.flags =
                         Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                    finish()
+                    startActivity(intent)
 
 
                 }
@@ -69,6 +82,16 @@ class AddSnakeLog : AppCompatActivity() {
         }
 
     }
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        if (currentFocus != null) {
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
+        }
+        return super.dispatchTouchEvent(ev)
+    }
+
+    //TODO override onbackpressed()
 
 
 
